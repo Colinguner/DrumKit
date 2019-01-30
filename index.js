58bd5f09e99db637c8d6f1531293caede05d6d0f
Colinguner <47030477+Colinguner@users.noreply.github.com>
2019-01-30 22:31:49 +0100

    Update index.js

--- index.js
+++ index.js
@@ -85,7 +85,7 @@ for (var i = 0; i < numberOfDrumButtons; i++) {
 // --------------------------
 
 
-function keyStuff(event) {
+/*function keyStuff(event) {
 
     
 
@@ -133,53 +133,53 @@ function keyStuff(event) {
                  audio.play();
     }
 }
+*/
 
 
 
-
-// function keyStuff(event) {
-//     // --------------------------
-//     var x = event.charCode || event.keyCode;  // Get the Unicode value
-//     var y = String.fromCharCode(x);  // Convert the value into a character
+function keyStuff(event) {
+    // --------------------------
+    var x = event.charCode || event.keyCode;  // Get the Unicode value
+    var y = String.fromCharCode(x);  // Convert the value into a character
    
 
-//     console.log("You pressed " + y + " and its charCode is " + x);
-//     // display which key was pressed, along with its charCode, in an alert
+    console.log("You pressed " + y + " and its charCode is " + x);
+    // display which key was pressed, along with its charCode, in an alert
     
-//     // -----------------------------
-//     if (x == 119) {
-//         var audio = new Audio('sounds/crash.mp3');
-//                     audio.play();
-//         }
+    // -----------------------------
+    if (x == 119) {
+        var audio = new Audio('sounds/crash.mp3');
+                    audio.play();
+        }
     
-//     else if (x == 97) {
-//         var audio = new Audio('sounds/kick-bass.mp3');
-//                  audio.play();
-//         }
+    else if (x == 97) {
+        var audio = new Audio('sounds/kick-bass.mp3');
+                 audio.play();
+        }
     
-//     else if (x == 115) {
-//         var audio = new Audio('sounds/snare.mp3');
-//                  audio.play();
-//         }
+    else if (x == 115) {
+        var audio = new Audio('sounds/snare.mp3');
+                 audio.play();
+        }
     
-//     else if (x == 100) {
-//         var audio = new Audio('sounds/tom-1.mp3');
-//                  audio.play();
-//         }
-//     else if (x == 106) {
-//         var audio = new Audio('sounds/tom-2.mp3');
-//                  audio.play();
-//     }
+    else if (x == 100) {
+        var audio = new Audio('sounds/tom-1.mp3');
+                 audio.play();
+        }
+    else if (x == 106) {
+        var audio = new Audio('sounds/tom-2.mp3');
+                 audio.play();
+    }
 
-//     else if (x == 107) {
-//         var audio = new Audio('sounds/tom-3.mp3');
-//                  audio.play();
+    else if (x == 107) {
+        var audio = new Audio('sounds/tom-3.mp3');
+                 audio.play();
     
-//     }else if (x == 108) {
-//         var audio = new Audio('sounds/tom-4.mp3');
-//                  audio.play();
-//     }
-// }
+    }else if (x == 108) {
+        var audio = new Audio('sounds/tom-4.mp3');
+                 audio.play();
+    }
+}
 
 
 
