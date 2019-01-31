b260af7ea8bc2e7932820254b08754f0dd75d568
Colinguner <47030477+Colinguner@users.noreply.github.com>
2019-01-31 17:07:37 +0100

    Update index.js
    Fixing code's readability and adding animation on click or keypress.

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
-
+// ______________________________EventListeners START_____________________________
 var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 for (var i = 0; i < numberOfDrumButtons; i++) {
     document.querySelectorAll(".drum")[i].addEventListener("click", function () {
         var buttonInnerHtml = this.innerHTML;
-        sound(buttonInnerHtml);
-})
-}
-document.addEventListener("keypress", function(event){
-    sound(event.key);
-})
 
+        sound(buttonInnerHtml);
+        flashAnimation(buttonInnerHtml);
+});
+}
+
+document.addEventListener("keypress", function(event){
+    console.log(event.key);
+    sound(event.key);
+    flashAnimation(event.key);
+});
+// ______________________________EventListeners END_____________________________
+
+
+
+
+
+// ______________________________Sound Function START_____________________________
 function sound (key){
     switch (key){
         case "w":
@@ -40,7 +51,50 @@ function sound (key){
             var tom_4 = new Audio('sounds/tom-4.mp3');
             tom_4.play();
         break;
-     
+        
         default: console.log(buttonInnerHtml);
     }
 }
+// ______________________________Sound Function END_____________________________
+
+
+
+
+
+// _____________________________Color Change START -style_____________________________
+// function flashAnimation(key){
+//     document.querySelector("." + key).style.color= "white";
+//     setTimeout(function(){
+//     document.querySelector("." + key).style.color= "#DA0463";    
+//     }, 100);
+// }
+// _____________________________Color Change END_____________________________
+
+
+// _____________________________Color Change START -class_____________________________
+function flashAnimation(key){
+    document.querySelector("." + key).classList.add("pressed");
+    setTimeout(function(){
+    document.querySelector("." + key).classList.remove("pressed");    
+    }, 100);
+}
+// _____________________________Color Change END_____________________________
+
+
+
+
+//_____________________________Works but to complicated for now_____________________________
+// function sleep(ms) {
+//     return new Promise(resolve => setTimeout(resolve, ms));
+//   }
+  
+// function flashAnimation(key){
+//     // działa--- tylko dodaje
+//     // document.querySelector(key).setAttribute("color", "white");
+//     document.querySelector("." + key).style.color= "white";
+//     sleep(200).then(() => {
+//         document.querySelector("." + key).style.color= "#DA0463";
+//     });
+// }
+// _____________________________Works but to complicated for now_____________________________
+
